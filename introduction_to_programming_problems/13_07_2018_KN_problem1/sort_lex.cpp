e87c89c984e950350984fb87c46960889e469cac
Stiliyan Goranov <segoranov@gmail.com>
2020-08-31 22:58:25 +0300

    Solve without std::stack

--- introduction_to_programming_problems/13_07_2018_KN_problem1/sort_lex.cpp
+++ introduction_to_programming_problems/13_07_2018_KN_problem1/sort_lex.cpp
@@ -38,6 +38,48 @@ int compareLex(unsigned int lhs, unsigned int rhs) {
   return 0;
 }
 
+int numberOfDigits(int x) {
+  if (0 <= x && x <= 9) {
+    return 1;
+  }
+
+  return 1 + numberOfDigits(x / 10);
+}
+
+int getNthDigit(int x, int n) {
+  const int xNumDigits = numberOfDigits(x);
+  if (n > xNumDigits)
+    throw "Error: provided n is larger than the number of digits of x.";
+
+  const int numberOfTimesToDivideByTen = xNumDigits - n;
+  for (int i = 0; i < numberOfTimesToDivideByTen; ++i) {
+    x /= 10;
+  }
+  return x % 10;
+}
+
+int compareLex1(unsigned int lhs, unsigned int rhs) {
+  const int lhsNumDigits = numberOfDigits(lhs);
+  const int rhsNumDigits = numberOfDigits(rhs);
+
+  const int smallerNumDigits =
+      (lhsNumDigits <= rhsNumDigits) ? lhsNumDigits : rhsNumDigits;
+
+  for (int digitId = 1; digitId <= smallerNumDigits; digitId++) {
+    const int lhsDigit = getNthDigit(lhs, digitId);
+    const int rhsDigit = getNthDigit(rhs, digitId);
+    if (lhsDigit > rhsDigit) return 1;
+    if (rhsDigit > lhsDigit) return -1;
+  }
+
+  if (lhsNumDigits == rhsNumDigits) return 0;
+
+  if (smallerNumDigits == lhsNumDigits)
+    return -1;  // for example when we compare '123' and '1234'
+  else
+    return 1;  // for example when we compare '1234' and '123'
+}
+
 void swap(unsigned int& a, unsigned int& b) {
   unsigned int tmp = a;
   a = b;
@@ -48,7 +90,7 @@ void sortLex(unsigned int arr[], int n) {
   for (int i = 0; i < n - 1; i++) {
     int max = i;
     for (int j = i + 1; j < n; j++) {
-      if (compareLex(arr[max], arr[j]) == 1) {  // i.e. arr[j] > arr[max]
+      if (compareLex1(arr[max], arr[j]) == 1) {  // i.e. arr[j] > arr[max]
         max = j;
       }
     }
@@ -64,9 +106,17 @@ void printArr(unsigned int arr[], int size) {
 }
 
 int main() {
-  std::cout << compareLex(123, 9554) << std::endl;
-  std::cout << compareLex(123, 123) << std::endl;
-  std::cout << compareLex(9554, 123) << std::endl;
+  std::cout << compareLex1(123, 9554) << std::endl;
+  std::cout << compareLex1(123, 123) << std::endl;
+  std::cout << compareLex1(9554, 123) << std::endl;
+
+  std::cout << compareLex1(1000, 100) << std::endl;
+  std::cout << compareLex1(100, 1000) << std::endl;
+  std::cout << compareLex1(100, 100) << std::endl;
+
+  for (int i = 1; i <= 5; i++) {
+    std::cout << i << "th digit of 12345: " << getNthDigit(12345, i) << '\n';
+  }
 
   unsigned int arr[] = {13, 14, 7, 2018, 9, 0};
   unsigned int arrSize = 6;
