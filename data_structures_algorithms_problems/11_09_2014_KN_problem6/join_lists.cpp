841def9e1d594dadb12dad16a052477c843328d0
Stiliyan Goranov <segoranov@gmail.com>
2020-09-09 23:01:21 +0300

    Solve partially new problem - lists joining

---
+++ data_structures_algorithms_problems/11_09_2014_KN_problem6/join_lists.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <map>
+#include <utility>
+
+struct Node {
+  Node(int data) : data{data} {}
+  int data;
+  Node* next = nullptr;
+  Node* prev = nullptr;
+};
+
+void addElemAtEnd(Node*& list, int elem) {
+  if (!list) {
+    list = new Node{elem};
+    return;
+  }
+
+  auto it = list;
+  while (it->next != nullptr) {
+    it = it->next;
+  }
+
+  it->next = new Node{elem};
+  it->next->prev = it;
+}
+
+void printList(Node* list) {
+  std::cout << "[ ";
+  if (!list) return;
+  while (list) {
+    std::cout << list->data << ' ';
+    list = list->next;
+  }
+
+  std::cout << "]\n";
+}
+
+int calcDistanceToEnd(Node* node) {
+  if (!node) return 0;
+
+  int dist = 0;
+  while (node->next != nullptr) {
+    node = node->next;
+    ++dist;
+  }
+
+  return dist;
+}
+
+int calcDistanceToBeginning(Node* node) {
+  if (!node) return 0;
+
+  int dist = 0;
+  while (node->prev != nullptr) {
+    node = node->prev;
+    ++dist;
+  }
+
+  return dist;
+}
+
+bool join(Node*& L1, Node*& L2) {
+  using Distance = int;
+  std::map<Distance, std::pair<Node*, Node*>> mapDistanceToNodePair;
+
+  auto it1 = L1;
+  auto it2 = L2;
+
+  while (it1) {
+    it2 = L2;
+    while (it2) {
+      const int d1 = calcDistanceToEnd(it1);
+      const int d2 = calcDistanceToBeginning(it2);
+      if (it1->data == it2->data && d1 == d2) {
+        mapDistanceToNodePair[d1] = {it1, it2};
+      }
+      it2 = it2->next;
+    }
+    it1 = it1->next;
+  }
+
+  // If the map is empty, joining of lists is not possible
+  // Otherwise mapDistanceToNodePair[0] will contain the pair of nodes with
+  // minimum distance (the map is sorted by key)
+
+  if (mapDistanceToNodePair.empty()) {
+    return false;
+  }
+
+  const auto minDistance = mapDistanceToNodePair.begin()->first;
+  const auto joiningNodeInL1 = mapDistanceToNodePair.begin()->second.first;
+  const auto joiningNodeInL2 = mapDistanceToNodePair.begin()->second.second;
+
+  std::cout << "Joining lists. Distance = " << minDistance
+            << "; Data = " << joiningNodeInL1->data << '\n';
+
+  // Join the two lists
+
+  // forward it1 to the last node of L1
+  it1 = L1;
+  while (it1->next != nullptr) it1 = it1->next;
+
+  // make it2 point to beginning of L2
+  it2 = L2;
+
+  it1->next =
+      joiningNodeInL2;  // make last node of L1 point to 'joining' node in L2
+
+  it2->prev =
+      joiningNodeInL1;  // make first node of L2 point to 'joining' node in L1
+
+  return true;
+}
+
+int main() {
+  Node* L1 = nullptr;
+  addElemAtEnd(L1, 1);
+  addElemAtEnd(L1, 3);
+  addElemAtEnd(L1, 5);
+  addElemAtEnd(L1, 7);
+  addElemAtEnd(L1, 9);
+  addElemAtEnd(L1, 6);
+
+  std::cout << "L1: ";
+  printList(L1);
+
+  Node* L2 = nullptr;
+  addElemAtEnd(L2, 3);
+  addElemAtEnd(L2, 4);
+  addElemAtEnd(L2, 8);
+  addElemAtEnd(L2, 5);
+  addElemAtEnd(L2, 7);
+
+  std::cout << "L2: ";
+  printList(L2);
+
+  if (!join(L1, L2)) {
+    std::cout << "Failed to join lists.\n";
+  }
+}
